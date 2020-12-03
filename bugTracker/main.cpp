93f13f073095cb5fbde4efe3564aa862eb2dcc0f
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-12-03 12:36:00 -0500

    Initial Commit

---
+++ bugTracker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bugTracker
+//
+//  Created by Kevaughn Clarke on 12/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
