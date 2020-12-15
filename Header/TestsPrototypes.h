919e5651d8aadb0c0a6101d8a3dfa95b301a2a43
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-12-15 12:56:08 -0500

    temp welcome and show proj function

---
+++ Header/TestsPrototypes.h
@@ -0,0 +1,20 @@
+//
+//  TestsPrototypes.h
+//  bugTracker
+//
+//  Created by Kevaughn Clarke on 12/15/20.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef TestsPrototypes_h
+#define TestsPrototypes_h
+
+
+
+
+
+#endif /* TestsPrototypes_h */
--- bugTracker/main.cpp
+++ bugTracker/main.cpp
@@ -7,10 +7,40 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <string>
 #include <sqlite3.h>
 
+#include "../Header/TestsPrototypes.h"
+
+using namespace std;
+
+//MARK:- Temp
+
+
+
+
+
+//MARK:- Main
+
+void showProjects();
 
 int main(int argc, const char * argv[]) {
+
+    cout << "Bug Tracker\n";
+    
+    showProjects();
+    
+
     
     return 0;
+}
+
+//MARK:- Functions
+
+void showProjects(){
+    cout << "Projects\t\t\t\t\t\t\t# of strings\n";
+    cout << "-------------------------------------------------\n";
+    
+    //fetch projects:
+    
 }
