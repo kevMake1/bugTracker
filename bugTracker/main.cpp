1b2879736d823e5d92e2645cfe2cef4ce3d4137f
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-12-17 22:20:06 -0500

    testing

--- bugTracker/main.cpp
+++ bugTracker/main.cpp
@@ -51,24 +51,37 @@ int main(int argc, const char * argv[]) {
     
     
     //Create table
-    string sql = "CREATE TABLE PERSON("
-                "ID INT PRIMARY KEY     NOT NULL, "
-                "NAME           TEXT    NOT NULL, "
-                "SURNAME          TEXT     NOT NULL, "
-                "AGE            INT     NOT NULL, "
-                "ADDRESS        CHAR(50), "
-                "SALARY         REAL );";
+    
+    string sql = "CREATE TABLE if not exists proj1("
+                "ID INT PRIMARY KEY, "
+                "BUGNAME           TEXT    NOT NULL, "
+                "DESCRIPTION       TEXT, "
+                "FIXED             BOOL     NOT NULL, "
+                "SOLUTION          TEXT ); ";
+            
     
     createTable(DB, sql);
     
+    //MARK:- test
+    //string testt = "SELECT name FROM test.sqlite_master WHERE type='table';";
+    string testt = "SELECT name FROM sqlite_master WHERE type='table';";
+    string t;
+    sqlite3_exec(DB, testt.c_str(), callback, NULL, NULL);
+    
+    
+    
+    //MARK:-
+    
+
     //Insert
     sql = "INSERT INTO PERSON VALUES(1, 'STEVE', 'GATES', 30, 'PALO ALTO', 1000.0);"
                    "INSERT INTO PERSON VALUES(2, 'BILL', 'ALLEN', 20, 'SEATTLE', 300.22);"
                    "INSERT INTO PERSON VALUES(3, 'PAUL', 'JOBS', 24, 'SEATTLE', 9900.0);";
-    insertDatabase(DB, sql);
+    sql = "INSERT INTO proj1 VALUES(NULL, 'bug1', 'some desc', 0";
+    //insertDatabase(DB, sql);
     
     string query = "SELECT * FROM PERSON;";
-    displayFromTable(DB, query);
+    //displayFromTable(DB, query);
     
     //delete
     sql = "DELETE FROM PERSON WHERE ID = 2;";
@@ -117,6 +130,8 @@ static int callback(void* data, int argc, char** argv, char** azColName){
 
 
 void createTable(sqlite3 *db, string sql){
+    
+    
     
     //Create table
     char *messageErr;
