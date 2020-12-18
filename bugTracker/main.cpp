217300a7066e446982353a425193fa728fa2e56f
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-12-17 23:35:20 -0500

    createProject finished

--- bugTracker/main.cpp
+++ bugTracker/main.cpp
@@ -28,7 +28,7 @@ vector<Project> projDatabase = {Project("test1"), Project("test2")};
 
 void showProjects();
 static int callback(void* data, int argc, char** argv, char** azColName);
-void createProject(sqlite3 *db, string sql);
+void createProject(sqlite3 *db, string projectName);
 void insertDatabase(sqlite3 *db, string sql);
 void displayFromTable(sqlite3 *db, string query);
 
@@ -50,17 +50,6 @@ int main(int argc, const char * argv[]) {
     }
     
     
-    //Create table
-    
-    string sql = "CREATE TABLE if not exists proj1("
-                "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "BUGNAME           TEXT    NOT NULL, "
-                "DESCRIPTION       TEXT, "
-                "FIXED             BOOL     NOT NULL, "
-                "SOLUTION          TEXT ); ";
-            
-    
-    //createTable(DB, sql);
     
     //MARK:- test
     //string testt = "SELECT name FROM test.sqlite_master WHERE type='table';";
@@ -74,11 +63,10 @@ int main(int argc, const char * argv[]) {
     
 
     //Insert
-    sql = "INSERT INTO PERSON VALUES(1, 'STEVE', 'GATES', 30, 'PALO ALTO', 1000.0);"
-                   "INSERT INTO PERSON VALUES(2, 'BILL', 'ALLEN', 20, 'SEATTLE', 300.22);"
-                   "INSERT INTO PERSON VALUES(3, 'PAUL', 'JOBS', 24, 'SEATTLE', 9900.0);";
-    sql = "INSERT INTO proj1(BUGNAME, DESCRIPTION, FIXED) VALUES( 'bug2', 'some desc', 0)";
-    insertDatabase(DB, sql);
+    //sql = "INSERT INTO PERSON VALUES(1, 'STEVE', 'GATES', 30, 'PALO ALTO', 1000.0);"
+
+    //sql = "INSERT INTO proj1(BUGNAME, DESCRIPTION, FIXED) VALUES( 'bug2', 'some desc', 0)";
+    //insertDatabase(DB, sql);
     //insertDatabase(DB, sql);
     
     string query = "SELECT * FROM PERSON;";
@@ -86,7 +74,7 @@ int main(int argc, const char * argv[]) {
     displayFromTable(DB, query);
     
     //delete
-    sql = "DELETE FROM PERSON WHERE ID = 2;";
+    //sql = "DELETE FROM PERSON WHERE ID = 2;";
     
     sqlite3_close(DB);
     
@@ -131,9 +119,14 @@ static int callback(void* data, int argc, char** argv, char** azColName){
 
 
 
-void createProject(sqlite3 *db, string sql){
+void createProject(sqlite3 *db, string projectName){
     
-    
+    string sql = "CREATE TABLE if not exists " + projectName + "("
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "BUGNAME           TEXT    NOT NULL, "
+                "DESCRIPTION       TEXT, "
+                "FIXED             BOOL     NOT NULL, "
+                "SOLUTION          TEXT ); ";
     
     //Create table
     char *messageErr;
