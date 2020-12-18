0015fefaef654f819590c2a3e1176a69ed9032fd
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-12-17 23:23:26 -0500

    createTable to createProject

--- bugTracker/main.cpp
+++ bugTracker/main.cpp
@@ -28,7 +28,7 @@ vector<Project> projDatabase = {Project("test1"), Project("test2")};
 
 void showProjects();
 static int callback(void* data, int argc, char** argv, char** azColName);
-void createTable(sqlite3 *db, string sql);
+void createProject(sqlite3 *db, string sql);
 void insertDatabase(sqlite3 *db, string sql);
 void displayFromTable(sqlite3 *db, string query);
 
@@ -53,14 +53,14 @@ int main(int argc, const char * argv[]) {
     //Create table
     
     string sql = "CREATE TABLE if not exists proj1("
-                "ID INT PRIMARY KEY, "
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
                 "BUGNAME           TEXT    NOT NULL, "
                 "DESCRIPTION       TEXT, "
                 "FIXED             BOOL     NOT NULL, "
                 "SOLUTION          TEXT ); ";
             
     
-    createTable(DB, sql);
+    //createTable(DB, sql);
     
     //MARK:- test
     //string testt = "SELECT name FROM test.sqlite_master WHERE type='table';";
@@ -77,11 +77,13 @@ int main(int argc, const char * argv[]) {
     sql = "INSERT INTO PERSON VALUES(1, 'STEVE', 'GATES', 30, 'PALO ALTO', 1000.0);"
                    "INSERT INTO PERSON VALUES(2, 'BILL', 'ALLEN', 20, 'SEATTLE', 300.22);"
                    "INSERT INTO PERSON VALUES(3, 'PAUL', 'JOBS', 24, 'SEATTLE', 9900.0);";
-    sql = "INSERT INTO proj1 VALUES(NULL, 'bug1', 'some desc', 0";
+    sql = "INSERT INTO proj1(BUGNAME, DESCRIPTION, FIXED) VALUES( 'bug2', 'some desc', 0)";
+    insertDatabase(DB, sql);
     //insertDatabase(DB, sql);
     
     string query = "SELECT * FROM PERSON;";
-    //displayFromTable(DB, query);
+    query = "SELECT BUGNAME FROM proj1 WHERE ID = 2;";
+    displayFromTable(DB, query);
     
     //delete
     sql = "DELETE FROM PERSON WHERE ID = 2;";
@@ -129,7 +131,7 @@ static int callback(void* data, int argc, char** argv, char** azColName){
 
 
 
-void createTable(sqlite3 *db, string sql){
+void createProject(sqlite3 *db, string sql){
     
     
     
